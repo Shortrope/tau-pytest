c8b0ff798a404de9a23e464b4273cb1ae3250505
Mario Kaack <mario@shortrope.com>
2022-08-08 20:29:35 -0700

    Parametrize tests

--- tests/test_math.py
+++ tests/test_math.py
@@ -17,3 +17,17 @@ def test_devide_by_zero():
         num = 1 / 0
 
     assert "division by zero" in str(e.value)
+
+
+products = [
+    (3, 6, 18),
+    (1, 99, 99),
+    (0, 33, 0),
+    (4, -9, -36),
+    (-8, -2, 16),
+    (2.5, 6.7, 16.75)
+]
+
+@pytest.mark.parametrize('a,b,p',products)
+def test_multiplication(a,b,p):
+    assert a * b == p
